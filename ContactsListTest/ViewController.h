f570e40aa9df9f4a3560e10c26dd1b9a501f5d6e
Vladimir Gordienko <vladimir.legacy@gmail.com>
2018-03-31 13:09:45 +0300

    Initial Commit

---
+++ ContactsListTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactsListTest
+//
+//  Created by Владимир Гордиенко on 31.03.2018.
+//  Copyright © 2018 Vladimir Gordienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
