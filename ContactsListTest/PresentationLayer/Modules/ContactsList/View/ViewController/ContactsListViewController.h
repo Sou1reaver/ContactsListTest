09e1b736c1bda413f07a2c41a37fe2b7b0fbe97c
Vladimir Gordienko <vladimir.legacy@gmail.com>
2018-04-01 13:19:09 +0300

    update comments

--- ContactsListTest/PresentationLayer/Modules/ContactsList/View/ViewController/ContactsListViewController.h
+++ ContactsListTest/PresentationLayer/Modules/ContactsList/View/ViewController/ContactsListViewController.h
@@ -1,6 +1,6 @@
 //
 //  ContactsListViewController.h
-//  DistilleryTestApp
+//  ContactsListTest
 //
 //  Created by Vladimir Gordienko on 01/04/2018.
 //  Copyright © 2018 Vladimir Gordienko. All rights reserved.
